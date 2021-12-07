f52d4ffd23baf16de9ad6f28bb27da9384b876d0
thyago_fontenele <thyago.fontenele@injunior.com.br>
2021-12-07 03:48:48 -0300

    pesquisando depois de apertar o botao, feito

--- PokéDex/pokemons/src/App.js
+++ PokéDex/pokemons/src/App.js
@@ -2,15 +2,16 @@ import React, {useState, useEffect} from "react";
 import classes from "./App.module.css";
 import api from './service/Api';
 import DisplayPokemon from './components/DisplayPokemon/DisplayPokemon';
-import Stats from './pages/Stats/Stats'
+
 function App() {
 
   const [pokemons, setPokemons] = useState([]);
   const [busca, setBusca ] = useState('');
+  const [pesquisa, setPesquisa ] = useState('');
   
 
   const getPokemons = () => {
-    api.get(`pokemon?limit=100&offset=0`)
+    api.get(`pokemon?limit=900&offset=0`)
     .then((res) => {
       setPokemons(res.data.results);
     
@@ -25,23 +26,23 @@ function App() {
   
   return (
     <>
+      
       <h1 className={classes.title} >Pokédex</h1>
   
       <div className={classes.pesquisa}>
 
-       <input type="text" className={classes.input} value={busca} onChange={ e => setBusca(e.target.value) }  placeholder='Pesquise o Pokémon'/>
+       <input type="text" className={classes.input} value={busca} onChange={ e => setBusca(e.target.value) }  placeholder='Nome do Pokémon'/>
 
-        <button className={classes.pesquisar}><img src="https://imagensemoldes.com.br/wp-content/uploads/2020/07/Lupa-Azul-PNG.png" className={classes.img} alt='lupa'/>
+        <button className={classes.pesquisar} onClick={ () => { setPesquisa(busca); } } ><img src="https://imagensemoldes.com.br/wp-content/uploads/2020/07/Lupa-Azul-PNG.png" className={classes.img} alt='lupa'/>
         </button>
       </div>
      
       <div className={classes.grid}>
 
-        {pokemons.filter((ele) =>{
-          if(busca === ''){
-            return ele;
-          }else if(ele.name.toLowerCase().includes(busca.toLowerCase())) {
-      
+        {pokemons.filter((ele) => {
+          if(pesquisa === ''){
+            
+          }else if(ele.name.toLowerCase().includes(pesquisa.toLowerCase())) {
             return ele;
           }
         }).map( (ele) => (
